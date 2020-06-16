9400d5bb6d7fdbc7e0b94233de22a1242e980597
Tomek Stach <tomasz.stach@gmail.com>
2020-06-16 20:28:21 +0200

    Add test codes

--- src/FreshMail/Client.php
+++ src/FreshMail/Client.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace FreshMail\ApiV2;
 
@@ -15,116 +17,116 @@ use Psr\Log\LoggerInterface;
  */
 class Client
 {
-    const HOST   = 'api.freshmail.com';
-    const SCHEME = 'https';
-    const PREFIX = 'rest';
-    const VERSION = 'v2';
+  const HOST   = 'api.freshmail.com';
+  const SCHEME = 'https';
+  const PREFIX = 'rest';
+  const VERSION = 'v2';
 
-    const CLIENT_VERSION = '3.0';
+  const CLIENT_VERSION = '3.0';
 
-    /**
-     * Bearer Token for authorization
-     * @var string
-     */
-    private $bearerToken;
+  /**
+   * Bearer Token for authorization
+   * @var string
+   */
+  private $bearerToken;
 
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
+  /**
+   * @var LoggerInterface
+   */
+  private $logger;
 
-    /**
-     * @var \GuzzleHttp\Client
-     */
-    private $guzzle;
+  /**
+   * @var \GuzzleHttp\Client
+   */
+  private $guzzle;
 
-    /**
-     * Client constructor.
-     * @param string $bearerToken
-     */
-    public function __construct($bearerToken = '')
-    {
-        $this->bearerToken = $bearerToken;
-        $this->logger = MonologFactory::createInstance();
-        $this->guzzle = new \GuzzleHttp\Client();
+  /**
+   * Client constructor.
+   * @param string $bearerToken
+   */
+  public function __construct($bearerToken = '')
+  {
+    $this->bearerToken = $bearerToken;
+    $this->logger = MonologFactory::createInstance();
+    $this->guzzle = new \GuzzleHttp\Client();
+  }
+
+  /**
+   * @param $uri
+   * @param array $params
+   * @return array
+   * @throws Exception
+   */
+  public function doRequest(string $uri, array $params = [])
+  {
+    try {
+      $method = ($params) ? 'POST' : 'GET';
+
+      $response = $this->guzzle->request($method, $uri, $this->getRequestOptions($params));
+      $rawResponse = $response->getBody()->getContents();
+      $jsonResponse = json_decode($rawResponse, true);
+
+      if (!$jsonResponse) {
+        throw new ServerException(sprintf('Unable to parse response from server, raw response: %s', $rawResponse));
+      }
+
+      return $jsonResponse;
+    } catch (\GuzzleHttp\Exception\ClientException $exception) {
+      if ($exception->getCode() == 401) {
+        throw new UnauthorizedException('Request unauthorized');
+      }
+
+      throw new \FreshMail\ApiV2\ClientException(sprintf('Connection error, error message: ' . $exception->getMessage()), 1);
+    } catch (\GuzzleHttp\Exception\ConnectException $exception) {
+      throw new ConnectionException(sprintf('Connection error, error message: ' . $exception->getMessage()), 2);
     }
+  }
 
-    /**
-     * @param $uri
-     * @param array $params
-     * @return array
-     * @throws Exception
-     */
-    public function doRequest(string $uri, array $params = [])
-    {
-        try {
-            $method = ($params) ? 'POST' : 'GET';
+  /**
+   * @param LoggerInterface $logger
+   */
+  public function setLogger(LoggerInterface $logger)
+  {
+    $this->logger = $logger;
+  }
 
-            $response = $this->guzzle->request($method, $uri, $this->getRequestOptions($params));
-            $rawResponse = $response->getBody()->getContents();
-            $jsonResponse = json_decode($rawResponse, true);
+  /**
+   * @param \GuzzleHttp\Client $guzzle
+   */
+  public function setGuzzleHttpClient(\GuzzleHttp\Client $guzzle)
+  {
+    $this->guzzle = $guzzle;
+  }
 
-            if (!$jsonResponse) {
-                throw new ServerException(sprintf('Unable to parse response from server, raw response: %s', $rawResponse));
-            }
+  /**
+   * @return array
+   */
+  private function getRequestOptions(array $requestData): array
+  {
+    return [
+      'base_uri' => sprintf('%s://%s/%s/', self::SCHEME, self::HOST, self::PREFIX),
+      RequestOptions::BODY => json_encode($requestData),
+      RequestOptions::HEADERS => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' => 'Bearer ' . $this->bearerToken,
+        'User-Agent' => $this->createUserAgent()
+      ]
+    ];
+  }
 
-            return $jsonResponse;
-        } catch (\GuzzleHttp\Exception\ClientException $exception) {
-            if ($exception->getCode() == 401) {
-                throw new UnauthorizedException('Request unauthorized');
-            }
-
-            throw new \FreshMail\ApiV2\ClientException(sprintf('Connection error, error message: '.$exception->getMessage()));
-        } catch (\GuzzleHttp\Exception\ConnectException $exception) {
-            throw new ConnectionException(sprintf('Connection error, error message: '.$exception->getMessage()));
-        }
-    }
-
-    /**
-     * @param LoggerInterface $logger
-     */
-    public function setLogger(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
-
-    /**
-     * @param \GuzzleHttp\Client $guzzle
-     */
-    public function setGuzzleHttpClient(\GuzzleHttp\Client $guzzle)
-    {
-        $this->guzzle = $guzzle;
-    }
-
-    /**
-     * @return array
-     */
-    private function getRequestOptions(array $requestData): array
-    {
-        return [
-            'base_uri' => sprintf('%s://%s/%s/', self::SCHEME, self::HOST, self::PREFIX),
-            RequestOptions::BODY => json_encode($requestData),
-            RequestOptions::HEADERS => [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer ' . $this->bearerToken,
-                'User-Agent' => $this->createUserAgent()
-            ]
-        ];
-    }
-
-    /**
-     * @return string
-     */
-    private function createUserAgent(): string
-    {
-        return
-            sprintf(
-                'freshmail/php-api-v2-client:%s;guzzle:%s;php:%s;interface:%s',
-                self::VERSION,
-                self::CLIENT_VERSION,
-                PHP_VERSION,
-                php_sapi_name()
-            );
-    }
+  /**
+   * @return string
+   */
+  private function createUserAgent(): string
+  {
+    return
+      sprintf(
+        'freshmail/php-api-v2-client:%s;guzzle:%s;php:%s;interface:%s',
+        self::VERSION,
+        self::CLIENT_VERSION,
+        PHP_VERSION,
+        php_sapi_name()
+      );
+  }
 }
